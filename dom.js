6cee320fec57b3936d191be50c507aa8f9d0aa21
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-06-29 12:33:15 +0530

    dom modify

--- dom.js
+++ dom.js
@@ -12,4 +12,60 @@ var itemList=document.querySelector('#items');
 //childNodes
 
 //console.log(itemList.childNodes)
-console.log(itemList.children)
+//console.log(itemList.children);
+//itemList.children[1].style.backgroundColor='yellow';
+
+//FirstChild
+
+// console.log(itemList.firstChild);
+
+ //firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello 1';
+
+//lastElementChild
+
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='hello 4';
+
+ //nextsibling
+
+// console.log(itemList.nextSibling);
+
+ //nextElementSibling\
+
+// console.log(itemList.nextElementSibling);
+
+ //previoussibling
+
+// console.log(itemList.previousSibling);
+
+//previousElementSibling
+
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.textContent='Hello';
+
+//createelement
+
+//create a div
+var mydiv=document.createElement('div');
+
+//Add class
+
+mydiv.className='hello';
+
+//add id
+mydiv.id='hello1';
+mydiv.setAttribute('title','Hello Div');
+
+console.log(mydiv);
+//create text node
+var mydivText=document.createTextNode('Hello World');
+
+//add  text to div
+
+mydiv.appendChild(mydivText);
+var container = document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+container.insertBefore(mydiv,h1);
