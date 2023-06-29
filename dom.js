f31be4b8f44b42ed212dce4d66f99ffe06593234
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-06-29 11:24:34 +0530

    modifying dom

---
+++ dom.js
@@ -0,0 +1,15 @@
+//traversing the DOM
+var itemList=document.querySelector('#items');
+//parentnode
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor='green';
+
+//parentElement
+
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor='green';
+
+//childNodes
+
+//console.log(itemList.childNodes)
+console.log(itemList.children)
