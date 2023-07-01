7e74d31d5b8016b0cf344ea60d991ffe2c92b78a
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-07-01 12:13:59 +0530

    add description

--- dom.js
+++ dom.js
@@ -15,6 +15,7 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
@@ -22,20 +23,25 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(description));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
+  var editbtn=document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
+  editbtn.className='btn btn-danger btn-sm float-right delete';
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  editbtn.appendChild(document.createTextNode('Edit'));
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(editbtn);
 
   // Append li to list
+  itemList.appendChild(li);
   itemList.appendChild(li);
 }
 
@@ -49,19 +55,20 @@ function removeItem(e){
   }
 }
 
-// Filter Items
-// function filterItems(e){
-//   // convert text to lowercase
-//   var text = e.target.value.toLowerCase();
-//   // Get lis
-//   var items = itemList.getElementsByTagName('li');
-//   // Convert to an array
-//   Array.from(items).forEach(function(item){
-//     var itemName = item.firstChild.textContent;
-//     if(itemName.toLowerCase().indexOf(text) != -1){
-//       item.style.display = 'block';
-//     } else {
-//       item.style.display = 'none';
-//     }
-//   });
-// }
+//Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
